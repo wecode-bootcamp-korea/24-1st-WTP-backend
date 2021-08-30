fb841c10fe89fe33ff53b1b6cb6a67ffde3e4c09
Jinatra <jinatra@Jinatraui-MacBookAir.local>
2021-08-30 16:51:15 +0900

    Added: Project & Initial Setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DATABASE 명',
+        'USER': 'root',
+        'PASSWORD': 'DB접속용 비밀번호',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-iltywe74xqzt4)posx2-jxyq$_t_i4i&du&0lga!wqdpvvdeh+'
