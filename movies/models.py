6e472fe5ad7b0e886a47349f71979c395c948d2e
Melody's lap top <melody@Melodysui-MacBookPro.local>
2021-09-01 14:08:31 +0900

    Modified:modeling_V2

--- movies/models.py
+++ movies/models.py
@@ -4,17 +4,17 @@ from users.models import User
 
 
 class Movie(models.Model):
-
     title          = models.CharField(max_length=100)
     release_date   = models.DateField()
     description    = models.TextField()
     running_time   = models.IntegerField()
-    average_rating = models.FloatField()
-    grade          = models.ForeignKey("Grade", on_delete=models.CASCADE)
-    poster_image   = models.CharField(max_length=300)
+    average_rating = models.DecimalField(max_digits=2, decimal_places=1)
+    grade          = models.ForeignKey("Grade", on_delete=models.SET_NULL)
+    poster_image   = models.URLField(max_length=300)
     trailer        = models.CharField(max_length=300)
-    participant    = models.ManyToManyField("Participant")
-    user           = models.ManyToManyField(User)
+    participant    = models.ManyToManyField("Participant", through="MovieParticipant")
+    user           = models.ManyToManyField(User, through="Rating", related_name="rater")
+    user           = models.ManyToManyField(User, through="WishList", related_name="wisher")
     genre          = models.ManyToManyField("Genre")
     country        = models.ManyToManyField("Country")
 
@@ -23,7 +23,6 @@ class Movie(models.Model):
 
 
 class Grade(models.Model):
-    
     grade = models.CharField(max_length=45)
     
     class Meta:
@@ -31,16 +30,14 @@ class Grade(models.Model):
 
 
 class Participant(models.Model):
-    
     name      = models.CharField(max_length=45)
-    image_url = models.CharField(max_length=500)
+    image_url = models.URLField(max_length=300)
 
     class Meta:
         db_table = "participants"
 
 
 class MovieParticipant(models.Model):
-    
     movie       = models.ForeignKey("Movie", on_delete=models.CASCADE)
     participant = models.ForeignKey("Participant", on_delete=models.CASCADE)
     role        = models.CharField(max_length=10)
@@ -50,10 +47,9 @@ class MovieParticipant(models.Model):
 
 
 class Rating(models.Model):
-   
     user    = models.ForeignKey(User, on_delete=models.CASCADE)
     movie   = models.ForeignKey("Movie", on_delete=models.CASCADE)
-    rate    = models.FloatField()
+    rate    = models.DecimalField(max_digits=2, decimal_places=1)
     comment = models.CharField(max_length=300)
 
     class Meta:
@@ -61,17 +57,15 @@ class Rating(models.Model):
 
 
 class WishList(models.Model):
-    
     user         = models.ForeignKey(User, on_delete=models.CASCADE)
     movie        = models.ForeignKey("Movie", on_delete=models.CASCADE)
-    created_date = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "wish_lists"
 
 
 class Genre(models.Model):
-    
     name = models.CharField(max_length=45)
 
     class Meta:
@@ -79,7 +73,6 @@ class Genre(models.Model):
 
 
 class MovieGenre(models.Model):
-    
     movie = models.ForeignKey("Movie", on_delete=models.CASCADE)
     genre = models.ForeignKey("Genre", on_delete=models.CASCADE)
 
@@ -88,8 +81,7 @@ class MovieGenre(models.Model):
 
 
 class Image(models.Model):
-    
-    image_url = models.CharField(max_length=500)
+    image_url = models.URLField(max_length=300)
     movie     = models.ForeignKey("Movie", on_delete=models.CASCADE)
 
     class Meta:
@@ -97,7 +89,6 @@ class Image(models.Model):
 
 
 class Country(models.Model):
-    
     name = models.CharField(max_length=45)
 
     class Meta:
@@ -105,7 +96,6 @@ class Country(models.Model):
 
 
 class MovieCountry(models.Model):
-    
     movie   = models.ForeignKey("Movie", on_delete=models.CASCADE)
     country = models.ForeignKey("Country", on_delete=models.CASCADE)
 
