638fd1f7daf774241362de0b5b3a96534e0c6b69
Jinatra <jinatra@Jinatraui-MacBookAir.local>
2021-09-03 11:19:19 +0900

    Edited: users views.py

--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@ import re
 import bcrypt
 
 from django.http.response   import JsonResponse
-from django.shortcuts       import render
 from django.views           import View
 from django.http            import JsonResponse
 
@@ -14,27 +13,27 @@ class SignUpView(View):
     def post(self, request):
         try:
             data             = json.loads(request.body)
-            sign_up_name     = data['name']
-            sign_up_email    = data['email']
-            sign_up_password = data['password']
+            name     = data['name']
+            email    = data['email']
+            password = data['password']
 
-            if not re.match('^[a-zA-Z가-힣]{2,}$', sign_up_name):
+            if not re.match('^[a-zA-Z가-힣]{2,}$', name):
                 return JsonResponse({'MESSAGE' : 'Wrong Name Form'}, status = 400)
 
-            if not re.match('^[a-zA-Z\d+-.]+@[a-zA-Z\d+-.]+\.[a-zA-Z]{2,3}$', sign_up_email):
+            if not re.match('^[a-zA-Z\d+-.]+@[a-zA-Z\d+-.]+\.[a-zA-Z]{2,3}$', email):
                 return JsonResponse({'MESSAGE' : 'Wrong E-mail Form'}, status = 400)
 
-            if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*-_])[A-Za-z\d!@#$%^&*-_]{10,}$',sign_up_password):
+            if not re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*-_])[A-Za-z\d!@#$%^&*-_]{10,}$',password):
                 return JsonResponse({'MESSAGE' : 'Wrong Password Form'}, status = 400)
 
-            if User.objects.filter(email = sign_up_email).exists():
+            if User.objects.filter(email = email).exists():
                 return JsonResponse({'MESSAGE':'Existed E-Mail'}, status = 400)
 
-            decoded_password = bcrypt.hashpw(sign_up_password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            decoded_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
             
             User.objects.create(
-                name     = sign_up_name,
-                email    = sign_up_email,
+                name     = name,
+                email    = email,
                 password = decoded_password,
             )
 
