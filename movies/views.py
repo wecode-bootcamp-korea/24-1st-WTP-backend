2797903c369125aa83605e532d53a6997260613c
root <root@peter.localdomain>
2021-09-03 11:14:49 +0900

    edit: login v3

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from django.views     import View
+
+from users.models import User
+from .models import *
+
+
+# 상세페이지
+class DetailView(View):
+    def get(self, request):
+        pass
--- users/utils.py
+++ users/utils.py
@@ -11,13 +11,16 @@ def login_decorator(func):
         try:
             access_token = request.headers.get("Authorization", None)
             user = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
+            
+            if not User.objects.get(id = user['id']):
+                return JsonResponse({'message' : '존재하지 않는 사용자입니다.'}, status=400)
+            
             request.user = User.objects.get(id = user['id'])
-
+        
         except jwt.exceptions.DecodeError:                                   
             return JsonResponse({'message' : '일치하지 않는 토큰입니다.' }, status=400)
 
-        except User.DoesNotExist:       
-            return JsonResponse({'message' : '존재하지 않는 사용자입니다.'}, status=400)
+        
 
         return func(self, request, *args, **kwargs)
 
