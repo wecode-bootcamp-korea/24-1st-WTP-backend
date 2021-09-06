35ed80b839f7c83b9b697c757af5176d37dd6fe9
Melody's lap top <melody@Melodysui-MacBookPro.local>
2021-09-06 20:00:57 +0900

    modify:MovieView_V7

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from movies.views import MovieView
+from movies.views import MovieView, RatingView
 
 urlpatterns = [
     path("movie",MovieView.as_view()),
+    path("user/<int:user_id>",RatingView.as_view())
 ]
