ee72422f4b997c73f9d19aa137b5540ec76bd5ae
root <root@peter.localdomain>
2021-09-06 17:04:04 +0900

    add: details2

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from movies.views import RateUpdate
+from movies.views import RateUpdate, RelatedMovie
 from users.views import Login
 
 
 urlpatterns = [
     path('/<int:movie_id>/rate', RateUpdate.as_view()),
+    path('/<int:movie_id>/related_movies', RelatedMovie.as_view()),
 ]
 
