e447a2cedbb55f3c04ee1ddd38794aa4452e0bbb
root <root@peter.localdomain>
2021-09-07 13:48:27 +0900

    login: conflict edit

--- users/views.py
+++ users/views.py
@@ -1,13 +1,14 @@
 import json
 import re
-import bcrypt
+import bcrypt, jwt
+from json.decoder import JSONDecodeError
 
 from django.http.response   import JsonResponse
 from django.views           import View
 from django.http            import JsonResponse
 
 from users.models           import User
-
+from my_settings      import SECRET_KEY, ALGORITHM
 
 class SignUpView(View):
     def post(self, request):
@@ -44,3 +45,31 @@ class SignUpView(View):
 
         except ValueError:
             return JsonResponse({'MESSAGE':'VALUE_ERROR'}, status = 400)
+
+
+class Login(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if not User.objects.filter(email=data['email']).exists():
+                return JsonResponse({"message": "존재하지 않는 아이디입니다!"}, status=401)
+
+            # db 안에 들어있는 비밀번호
+            current_user = User.objects.get(email=data['email'])
+            
+            if not bcrypt.checkpw(data['password'].encode(), current_user.password.encode()):
+                return JsonResponse({"message": "비밀번호가 일치하지 않습니다!"}, status=401)
+                
+            token = jwt.encode({"id": current_user.id}, SECRET_KEY, algorithm=ALGORITHM)
+
+            return JsonResponse({
+                "message": "accepted",
+                "auth_token": token,
+            }, status=200)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY ERROR"}, status=400)
+
+        except JSONDecodeError:
+            return JsonResponse({"message": "INVALID DATA FORMAT"}, status=400)
