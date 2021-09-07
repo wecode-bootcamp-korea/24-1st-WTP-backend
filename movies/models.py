71de0f5265dc33a95c1ef71ea8ddf208584a6ae4
Jinatra <jinatra@Jinatraui-MacBookAir.local>
2021-09-07 16:41:45 +0900

    임시

--- movies/models.py
+++ movies/models.py
@@ -51,7 +51,7 @@ class Rating(models.Model):
     user    = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     movie   = models.ForeignKey("Movie", on_delete=models.CASCADE)
     rate    = models.DecimalField(max_digits=2, decimal_places=1)
-    comment = models.CharField(max_length=300)
+    comment = models.CharField(max_length=1000)
 
     class Meta:
         db_table = "ratings"
--- movies/views.py
+++ movies/views.py
@@ -1,60 +1,61 @@
-from django.views     import View
-from django.http      import JsonResponse
+from django.views              import View
+from django.http.response      import JsonResponse
 
 from movies.models    import *
 
 class MovieDetailView(View):
     def get(self, request, movie_id):
-        
-        if not Movie.objects.filter(id = movie_id).exists():
-            return JsonResponse({'MESSAGE':'Movie Not Exists'}, status = 404)
+        try:
+            if not Movie.objects.filter(id = movie_id).exists():
+                return JsonResponse({'MESSAGE':'Movie Not Exists'}, status = 404)
 
-        movie = Movie.objects.get(id = movie_id)
+            movie = Movie.objects.get(id = movie_id)
 
-        title          = movie.title
-        release_date   = movie.release_date
-        # average_rating = movie.average_rating
-        poster_image   = movie.poster_image
-        trailer        = movie.trailer
-        description    = movie.description
+            title          = movie.title
+            release_date   = movie.release_date
+            poster_image   = movie.poster_image
+            trailer        = movie.trailer
+            description    = movie.description
 
-        # # 1
-        # image_url = [image.image_url for image in movie.image_set.all()]
+            image_url = [image.image_url for image in movie.image_set.all()]
 
-        # 2
-        image_url = []
-        images = Image.objects.filter(movie_id = movie_id)
-        for image in images:
-            image_url.append(image.image_url)
+            # # 2
+            # image_url = []
+            # images = Image.objects.filter(movie_id = movie_id)
+            # for image in images:
+            #     image_url.append(image.image_url)
 
-        participants = []
-        movie_participants = MovieParticipant.objects.filter(movie = movie_id)
-        for movie_participant in movie_participants:
-            participants.append(
-                {
-                    'name' : movie_participant.participant.name,
-                    'role' : movie_participant.role,
-                    'image' : movie_participant.participant.image_url
-                }
-            )
+            participants = []
+            movie_participants = MovieParticipant.objects.filter(movie = movie_id)
+            for movie_participant in movie_participants:
+                participants.append(
+                    {
+                        'name' : movie_participant.participant.name,
+                        'role' : movie_participant.role,
+                        'image' : movie_participant.participant.image_url
+                    }
+                )
 
-        rating_users  = [rating.user for rating in movie.rating_set.all()]
-        rates         = [rating.rate for rating in movie.rating_set.all()]
+            rating_users  = [rating.user for rating in movie.rating_set.all()]
+            rates         = [rating.rate for rating in movie.rating_set.all()]
 
-        average_rating = sum(rates)/len(rating_users)
+            average_rating = sum(rates)/len(rating_users)
 
-        movie_details = {
-            'title'          : title,
-            'release_date'   : release_date,
-            'genre'          : [genre.name for genre in movie.genre.all()],
-            'country'        : [country.name for country in movie.country.all()],
-            'poster_image'   : poster_image,
-            'trailer'        : trailer,
-            'image_url'      : image_url,
-            'description'    : description,
-            'participants'   : participants,
-            'average_rating' : average_rating,
-            'rating_users'   : len(rating_users),
-        }
+            movie_details = {
+                'title'          : title,
+                'release_date'   : release_date,
+                'genre'          : [genre.name for genre in movie.genre.all()],
+                'country'        : [country.name for country in movie.country.all()],
+                'poster_image'   : poster_image,
+                'trailer'        : trailer,
+                'image_url'      : image_url,
+                'description'    : description,
+                'participants'   : participants,
+                'average_rating' : average_rating,
+                'rating_users'   : len(rating_users),
+            }
 
-        return JsonResponse({'movie_info': movie_details}, status = 200)
+            return JsonResponse({'movie_info': movie_details}, status = 200)
+
+        except KeyError:
+            JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
