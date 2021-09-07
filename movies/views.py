b1a0891641d063b9916941934a38e5af5273e43a
Jinatra <jinatra@Jinatraui-MacBookAir.local>
2021-09-07 14:11:48 +0900

    Added: movie detail view, description & rating info

--- movies/views.py
+++ movies/views.py
@@ -13,9 +13,10 @@ class MovieDetailView(View):
 
         title          = movie.title
         release_date   = movie.release_date
-        average_rating = movie.average_rating
+        # average_rating = movie.average_rating
         poster_image   = movie.poster_image
         trailer        = movie.trailer
+        description    = movie.description
 
         # # 1
         # image_url = [image.image_url for image in movie.image_set.all()]
@@ -37,18 +38,23 @@ class MovieDetailView(View):
                 }
             )
 
-        print(participants)
+        rating_users  = [rating.user for rating in movie.rating_set.all()]
+        rates         = [rating.rate for rating in movie.rating_set.all()]
+
+        average_rating = sum(rates)/len(rating_users)
 
         movie_details = {
             'title'          : title,
             'release_date'   : release_date,
             'genre'          : [genre.name for genre in movie.genre.all()],
             'country'        : [country.name for country in movie.country.all()],
-            'average_rating' : average_rating,
             'poster_image'   : poster_image,
             'trailer'        : trailer,
             'image_url'      : image_url,
+            'description'    : description,
             'participants'   : participants,
+            'average_rating' : average_rating,
+            'rating_users'   : len(rating_users),
         }
 
         return JsonResponse({'movie_info': movie_details}, status = 200)
